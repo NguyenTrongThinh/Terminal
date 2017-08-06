f3dbeb8f93c738c0377f7162eccdb8378b07c147
ThinhNguyen <nguyentrongthinhit@gmail.com>
2017-08-06 14:18:52 +0700

    main.cpp : show full screen
    Signed-off-by: ThinhNguyen <nguyentrongthinhit@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
 #include "terminal.h"
 #include <QApplication>
-#include <QDesktopWidget>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Terminal w;
-    QDesktopWidget desktop;
 
-    int desktopHeight=desktop.geometry().height();
-    int desktopWidth=desktop.geometry().width();
-    w.setGeometry(0, 0, desktopWidth, desktopHeight);
-    w.show();
+
+
+    w.showFullScreen();
     return a.exec();
 }
